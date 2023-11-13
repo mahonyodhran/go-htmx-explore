5c8b18584f82c4fee456e521d126ea06d57c4e4a
Odhran Mahony <mahonyodhran@gmail.com>
2023-11-13 20:19:08 +0000

    added Films struct with data - shown in index.html

--- main.go
+++ main.go
@@ -6,12 +6,24 @@ import (
 	"net/http"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 	log.Println("Starting app...")
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, nil)
+		films := map[string][]Film{
+			"Films": {
+				{Title: "ET", Director: "Stephen Spielberg"},
+				{Title: "21 Jump Street", Director: "Phil Lord"},
+				{Title: "The Prestige", Director: "Christopher Nolan"},
+			},
+		}
+		tmpl.Execute(w, films)
 	}
 	http.HandleFunc("/", h1)
 
